8563113d251361976c7a82594d354d9d7b5de842
ThomasSchuetz <t.schuetz.1990@gmail.com>
2022-10-02 14:47:58 +0200

    enha: Add put logic

--- src/sortingPile.test.js
+++ src/sortingPile.test.js
@@ -1,26 +1,75 @@
 describe("SortingPile", () => {
-    it("IncreasingOrderStartsBelowMinValue", () => {
+    it("Increasing order starts below min value", () => {
         let pile = new SortingPile(1, 10, true);
 
         assert(pile.currentValue < 1);
     });
+    
+    it("Increasing order regular card", () => {
+        let pile = new SortingPile(1, 100, true);
 
-    it("DecreasingOrderStartsBelowMinValue", () => {
+        pile.put(50);
+
+        assert(pile.currentValue == 50);
+    });
+
+    it("Increasing order improvement card", () => {
+        let pile = new SortingPile(1, 100, true);
+        
+        pile.put(17);
+        pile.put(7);
+
+        assert(pile.currentValue == 7);
+    });
+
+    it("Increasing order non-improvement card throws", () => {
+        let pile = new SortingPile(1, 100, true);
+
+        pile.put(80);
+        
+        assert.throws(function () { pile.put(79) }, RangeError);
+    });
+
+    it("Decreasing order starts above max value", () => {
         let pile = new SortingPile(1, 100, false);
 
         assert(pile.currentValue > 100);
     });
+    
+    it("Decreasing order regular card", () => {
+        let pile = new SortingPile(1, 100, false);
 
-    // it("Draw cards until exception is thrown", () => {
-    //     let min = 1;
-    //     let max = 9;
-    //     let cardStack = new CardStack(min, max);
+        pile.put(50);
 
-    //     for (let i = 0; i <= max - min; i++) {
-    //         const card = cardStack.draw();
-    //         assertCard(card, min, max);
-    //     }
+        assert(pile.currentValue == 50);
+    });
 
-    //     assert.throws(function () { cardStack.draw() }, RangeError);
-    // });
+    it("Decreasing order improvement card", () => {
+        let pile = new SortingPile(1, 100, false);
+        
+        pile.put(17);
+        pile.put(27);
+
+        assert(pile.currentValue == 27);
+    });
+
+    it("Decreasing order non-improvement card throws", () => {
+        let pile = new SortingPile(1, 100, false);
+
+        pile.put(80);
+        
+        assert.throws(function () { pile.put(81) }, RangeError);
+    });
+
+    it("Card above limit throws", () => {
+        let pile = new SortingPile(1, 100, true);
+
+        assert.throws(function () { pile.put(101) }, RangeError);
+    });
+
+    it("Card below limit throws", () => {
+        let pile = new SortingPile(1, 100, true);
+
+        assert.throws(function () { pile.put(0) }, RangeError);
+    });
 })
